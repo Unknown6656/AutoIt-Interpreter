5568eaa00d11638e05f56386340145171076cf84
Unknown6656 <unknown84@gmx.de>
2018-04-22 00:52:49 +0200

    forgot to move test method to unit test project

--- UnitTests/ExpressionTests.cs
+++ UnitTests/ExpressionTests.cs
@@ -8,6 +8,17 @@ namespace UnitTests
         [TestMethod]
         public void Test_01()
         {
+            string str = "$ulk += 5 << ($lel == $lel) * ($jej = (99 << 3) + (8 / (7 + 2) - 6)) + \"lel\" ^ (14 >>> 3 + 88) nor ($lulz <> $lulz)";
+            var parser = new AutoItExpressionParser.ExpressionParser();
+
+            parser.Initialize();
+
+            var expr = (parser.Parse(str)[0] as AutoItExpressionParser.ExpressionAST.MULTI_EXPRESSION.SingleValue).Item;
+            var sexpr = AutoItExpressionParser.Refactorings.ProcessConstants(expr);
+
+            string e1 = AutoItExpressionParser.ExpressionAST.Print(expr);
+            string e2 = AutoItExpressionParser.ExpressionAST.Print(sexpr);
+
             // TODO
         }
     }
