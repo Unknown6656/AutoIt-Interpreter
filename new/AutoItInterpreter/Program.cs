5e61904b11e8228888efe0dd1f25dd003325b85c
unknown6656 <unknown84@gmx.de>
2020-05-31 14:06:23 +0200

    updated the project's metadata

---
+++ new/AutoItInterpreter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unknown6656.AutoIt3
+{
+    public static class Program
+    {
+        public static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
