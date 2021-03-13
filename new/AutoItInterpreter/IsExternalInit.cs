1fa68ddf3c96998792c783a5e97d6323a4c86f7f
unknown6656 <unknown84@gmx.de>
2021-03-14 00:28:25 +0100

    removed IsExternalInit

--- new/AutoItInterpreter/IsExternalInit.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
-
-namespace System.Runtime.CompilerServices
-{
-    /// <summary>For C#9 record compatibility.</summary>
-    [EditorBrowsable(EditorBrowsableState.Never), DebuggerNonUserCode]
-    public sealed class IsExternalInit { }
-}
