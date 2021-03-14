e8a4c0fdce98fd613aea9f1871cc02eb8e799360
unknown6656 <unknown84@gmx.de>
2021-03-14 21:07:13 +0100

    Added the NaturalDocs binaries

--- new/AutoItInterpreter/AssemblyInfo.cs
+++ new/AutoItInterpreter/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 
 //////////////////////////////////////////////////////////////////////////
-// Auto-generated 2021-03-14 00:27:46.187                               //
+// Auto-generated 2021-03-14 20:49:31.248                               //
 // ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
 //////////////////////////////////////////////////////////////////////////
 
 using System.Reflection;
 using System;
 
-[assembly: AssemblyVersion("0.8.1730.7549")]
-[assembly: AssemblyFileVersion("0.8.1730.7549")]
-[assembly: AssemblyInformationalVersion("v.0.8.1730.7549, commit: 151d143430138ca3176e91e1ef39261be7efc691")]
+[assembly: AssemblyVersion("0.8.1755.7549")]
+[assembly: AssemblyFileVersion("0.8.1755.7549")]
+[assembly: AssemblyInformationalVersion("v.0.8.1755.7549, commit: 0b04843f6e3a4c61da300dc93c9cc45897a8dcec")]
 [assembly: AssemblyCompany("Unknown6656")]
 [assembly: AssemblyCopyright("Copyright © 2018 - 2021, Unknown6656")]
 [assembly: AssemblyProduct("AutoIt-Interpreter by Unknown6656")]
@@ -35,11 +35,11 @@ public static class __module__
     /// <summary>
     /// The interpreter's current version.
     /// </summary>
-    public static Version? InterpreterVersion { get; } = Version.Parse("0.8.1730.7549");
+    public static Version? InterpreterVersion { get; } = Version.Parse("0.8.1755.7549");
     /// <summary>
     /// The Git hash associated with the current build.
     /// </summary>
-    public const string GitHash = "151d143430138ca3176e91e1ef39261be7efc691";
+    public const string GitHash = "0b04843f6e3a4c61da300dc93c9cc45897a8dcec";
     /// <summary>
     /// The name of the GitHub repository associated with <see cref="RepositoryURL"/>.
     /// </summary>
@@ -49,7 +49,7 @@ public static class __module__
     /// </summary>
     public const string RepositoryURL = "https://github.com/Unknown6656/AutoIt-Interpreter";
     /// <summary>
-    /// The date and time of the current build (2021-03-14 00:27:46.187).
+    /// The date and time of the current build (2021-03-14 20:49:31.248).
     /// </summary>
-    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01d71860794f195eL);
+    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01d7190b2687b59bL);
 }
