7166774752e6aa15eb55ff86fd7a810316935733
unknown6656 <unknown84@gmx.de>
2021-03-18 23:00:09 +0100

    decorated plugin with assembly attribute

--- new/plugins/Plugin.Au3Framework.WindowsSpecific/WindowsSpecificMacros.cs
+++ new/plugins/Plugin.Au3Framework.WindowsSpecific/WindowsSpecificMacros.cs
@@ -1,6 +1,10 @@
 ﻿using System.Runtime.InteropServices;
 
+using Unknown6656.AutoIt3.Extensibility;
 using Unknown6656.AutoIt3.Runtime;
+
+[assembly: AutoIt3Plugin]
+
 
 namespace Unknown6656.AutoIt3.Extensibility.Plugins.Au3Framework
 {
