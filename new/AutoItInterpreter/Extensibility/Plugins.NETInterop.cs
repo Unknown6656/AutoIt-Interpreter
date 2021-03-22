e6ce438d6474307aa98cb81ebfa7288c0394ac1a
unknown6656 <unknown84@gmx.de>
2021-03-22 22:09:16 +0100

    extended the .NET API

--- new/AutoItInterpreter/Extensibility/Plugins.NETInterop.cs
+++ new/AutoItInterpreter/Extensibility/Plugins.NETInterop.cs
@@ -10,9 +10,10 @@ namespace Unknown6656.AutoIt3.Extensibility
         {
             RegisterFunction(nameof(NETNew), 1, 256, NETNew); // <name> [opt-args]
             RegisterFunction(nameof(NETClass), 1, NETClass); // <name>
+            RegisterFunction(nameof(NETDelete), 1, NETDelete); // <handle>
         }
 
-        public static FunctionReturnValue NETNew(NativeCallFrame frame, Variant[] args)
+        public static FunctionReturnValue NETNew(CallFrame frame, Variant[] args)
         {
             args = frame.PassedArguments;
 
@@ -24,7 +25,7 @@ namespace Unknown6656.AutoIt3.Extensibility
                 return FunctionReturnValue.Error(1);
         }
 
-        public static FunctionReturnValue NETClass(NativeCallFrame frame, Variant[] args)
+        public static FunctionReturnValue NETClass(CallFrame frame, Variant[] args)
         {
             bool result = frame.Interpreter.GlobalObjectStorage.TryCreateNETStaticRefrence(args[0].ToString(), out Variant reference);
 
@@ -33,5 +34,7 @@ namespace Unknown6656.AutoIt3.Extensibility
             else
                 return FunctionReturnValue.Error(1);
         }
+
+        public static FunctionReturnValue NETDelete(CallFrame frame, Variant[] args) => (Variant)frame.Interpreter.GlobalObjectStorage.Delete(args[0]);
     }
 }
