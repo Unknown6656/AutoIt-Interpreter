84369544f5668e1a6215700aa31b9a826c5d85f6
Unknown6656 <unknown84@gmx.de>
2023-11-05 09:15:06 +0100

    re-build

--- new/AutoItInterpreter/AssemblyInfo.cs
+++ new/AutoItInterpreter/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 
 //////////////////////////////////////////////////////////////////////////
-// Auto-generated 2023-10-05 22:43:58.233                               //
+// Auto-generated 2023-11-05 09:14:27.864                               //
 // ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
 //////////////////////////////////////////////////////////////////////////
 
 using System.Reflection;
 using System;
 
-[assembly: AssemblyVersion("0.8.2066.8466")]
-[assembly: AssemblyFileVersion("0.8.2066.8466")]
-[assembly: AssemblyInformationalVersion("v.0.8.2066.8466, commit: <unknown>")]
+[assembly: AssemblyVersion("0.8.2073.8497")]
+[assembly: AssemblyFileVersion("0.8.2073.8497")]
+[assembly: AssemblyInformationalVersion("v.0.8.2073.8497, commit: 57ced8fa6526cedc74d8fb4f92834f9a6b8b87f4")]
 [assembly: AssemblyCompany("Unknown6656")]
 [assembly: AssemblyCopyright("Copyright © 2018 - 2023, Unknown6656")]
 [assembly: AssemblyProduct("AutoIt-Interpreter by Unknown6656")]
@@ -35,11 +35,11 @@ public static class __module__
     /// <summary>
     /// The interpreter's current version.
     /// </summary>
-    public static Version? InterpreterVersion { get; } = Version.Parse("0.8.2066.8466");
+    public static Version? InterpreterVersion { get; } = Version.Parse("0.8.2073.8497");
     /// <summary>
     /// The Git hash associated with the current build.
     /// </summary>
-    public const string GitHash = "<unknown>";
+    public const string GitHash = "57ced8fa6526cedc74d8fb4f92834f9a6b8b87f4";
     /// <summary>
     /// The name of the GitHub repository associated with <see cref="RepositoryURL"/>.
     /// </summary>
@@ -49,7 +49,7 @@ public static class __module__
     /// </summary>
     public const string RepositoryURL = "https://github.com/Unknown6656/AutoIt-Interpreter";
     /// <summary>
-    /// The date and time of the current build (2023-10-05 22:43:58.233).
+    /// The date and time of the current build (2023-11-05 09:14:27.864).
     /// </summary>
-    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01d9f80fb82d9250L);
+    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01da0fc0186ae968L);
 }
