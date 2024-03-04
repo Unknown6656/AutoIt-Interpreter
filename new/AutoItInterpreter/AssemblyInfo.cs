acde43b02c437f7619b8b64d5e3dae1c56b887f9
Unknown6656 <unknown84@gmx.de>
2024-03-04 13:56:54 +0100

    added auto-generated appveyor.yml

--- new/AutoItInterpreter/AssemblyInfo.cs
+++ new/AutoItInterpreter/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 
 //////////////////////////////////////////////////////////////////////////
-// Auto-generated 2024-03-04 10:26:37.838                               //
+// Auto-generated 2024-03-04 13:55:54.002                               //
 // ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
 //////////////////////////////////////////////////////////////////////////
 
 using System.Reflection;
 using System;
 
-[assembly: AssemblyVersion("0.10.2150.8608")]
-[assembly: AssemblyFileVersion("0.10.2150.8608")]
-[assembly: AssemblyInformationalVersion("v.0.10.2150.8608, commit: 81656e90ad4205e07dd4acb666c42e2f71b4e0ac")]
+[assembly: AssemblyVersion("0.10.2153.8608")]
+[assembly: AssemblyFileVersion("0.10.2153.8608")]
+[assembly: AssemblyInformationalVersion("v.0.10.2153.8608, commit: a39491f91012aa95f9cbdaf086b4f434df9c71b2")]
 [assembly: AssemblyCompany("Unknown6656")]
 [assembly: AssemblyCopyright("Copyright © 2018 - 2024, Unknown6656")]
 [assembly: AssemblyProduct("AutoIt-Interpreter by Unknown6656")]
@@ -35,11 +35,11 @@ public static class __module__
     /// <summary>
     /// The interpreter's current version.
     /// </summary>
-    public static Version? InterpreterVersion { get; } = Version.Parse("0.10.2150.8608");
+    public static Version? InterpreterVersion { get; } = Version.Parse("0.10.2153.8608");
     /// <summary>
     /// The Git hash associated with the current build.
     /// </summary>
-    public const string GitHash = "81656e90ad4205e07dd4acb666c42e2f71b4e0ac";
+    public const string GitHash = "a39491f91012aa95f9cbdaf086b4f434df9c71b2";
     /// <summary>
     /// The name of the GitHub repository associated with <see cref="RepositoryURL"/>.
     /// </summary>
@@ -49,7 +49,7 @@ public static class __module__
     /// </summary>
     public const string RepositoryURL = "https://github.com/Unknown6656/AutoIt-Interpreter";
     /// <summary>
-    /// The date and time of the current build (2024-03-04 10:26:37.838).
+    /// The date and time of the current build (2024-03-04 13:55:54.002).
     /// </summary>
-    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01da6e160eda6637L);
+    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01da6e334ae8fd4eL);
 }
--- new/util/VersionIncrementer/Program.cs
+++ new/util/VersionIncrementer/Program.cs
@@ -1,121 +1,134 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System;
-using System.Diagnostics;
 
-namespace VersionIncrementer
+namespace VersionIncrementer;
+
+
+public static class Program
 {
-    public static class Program
+    public const string REPOSITORY_AUTHOR = "Unknown6656";
+    public const string REPOSITORY_NAME = "AutoIt-Interpreter";
+    public const string REPOSITORY_URL = $"https://github.com/{REPOSITORY_AUTHOR}/{REPOSITORY_NAME}";
+    public const int START_YEAR = 2018;
+
+
+    public static void Main(string[] args)
     {
-        public const string REPOSITORY_AUTHOR = "Unknown6656";
-        public const string REPOSITORY_NAME = "AutoIt-Interpreter";
-        public const string REPOSITORY_URL = "https://github.com/" + REPOSITORY_AUTHOR + "/" + REPOSITORY_NAME;
-        public const int START_YEAR = 2018;
+        args[0] = args[0].TrimEnd('\\', '/');
 
+        string metapath = args[0] + "/../AssemblyInfo.cs";
+        string verspath = args[0] + "/../version.txt";
+        string appveyorpath = args[0] + "/../../../appveyor.yml";
+        string githash = "<unknown>";
+        string vers = "0.0.0.0";
 
-        public static void Main(string[] args)
+        if (File.Exists(verspath))
+            vers = File.ReadAllText(verspath).Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)[0].Trim();
+
+        if (!Version.TryParse(vers, out Version v1))
+            v1 = new Version(0, 0, 0, 0);
+
+        DateTime now = DateTime.Now;
+        Version v2 = new(v1.Major, v1.Minor, v1.Build + 1, (now.Year - 2000) * 356 + now.DayOfYear);
+
+        try
         {
-            string metapath = args[0] + "/../AssemblyInfo.cs";
-            string verspath = args[0] + "/../version.txt";
-            string githash = "<unknown>";
-            string vers = "0.0.0.0";
-
-            if (File.Exists(verspath))
-                vers = File.ReadAllText(verspath).Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)[0].Trim();
-
-            if (!Version.TryParse(vers, out Version v1))
-                v1 = new Version(0, 0, 0, 0);
-
-            DateTime now = DateTime.Now;
-            Version v2 = new(v1.Major, v1.Minor, v1.Build + 1, (now.Year - 2000) * 356 + now.DayOfYear);
-
-            try
+            using Process p = new()
             {
-                using Process p = new()
+                StartInfo = new ProcessStartInfo
                 {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "git",
-                        Arguments = "rev-parse HEAD",
-                        WorkingDirectory = new FileInfo(verspath).Directory.FullName,
-                        UseShellExecute = false,
-                        RedirectStandardOutput = true,
-                        CreateNoWindow = true
-                    }
-                };
+                    FileName = "git",
+                    Arguments = "rev-parse HEAD",
+                    WorkingDirectory = new FileInfo(verspath).Directory.FullName,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
 
-                p.Start();
+            p.Start();
 
-                githash = p.StandardOutput.ReadToEnd().Trim();
+            githash = p.StandardOutput.ReadToEnd().Trim();
 
-                p.WaitForExit();
-            }
-            catch
-            {
-            }
-
-            if (string.IsNullOrWhiteSpace(githash))
-                githash = "<unknown git commit hash>";
-
-            string year = START_YEAR < now.Year ? $"{START_YEAR} - {now.Year}" : START_YEAR.ToString();
-            string copyright = $"Copyright © {year}, {REPOSITORY_AUTHOR}";
-
-            File.WriteAllText(verspath, $"{v2}\n{githash}");
-            File.WriteAllText(metapath, $@"
-//////////////////////////////////////////////////////////////////////////
-// Auto-generated {now:yyyy-MM-dd HH:mm:ss.fff}                               //
-// ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
-//////////////////////////////////////////////////////////////////////////
-
-using System.Reflection;
-using System;
-
-[assembly: AssemblyVersion(""{v2}"")]
-[assembly: AssemblyFileVersion(""{v2}"")]
-[assembly: AssemblyInformationalVersion(""v.{v2}, commit: {githash}"")]
-[assembly: AssemblyCompany(""{REPOSITORY_AUTHOR}"")]
-[assembly: AssemblyCopyright(""{copyright}"")]
-[assembly: AssemblyProduct(""{REPOSITORY_NAME} by {REPOSITORY_AUTHOR}"")]
-[assembly: AssemblyTitle(""autoit3"")]
-
-/// <summary>
-/// A global module containing some meta-data.
-/// </summary>
-public static class __module__
-{{
-    /// <summary>
-    /// The interpreter's author. This value is equal to the author of the GitHub repository associated with <see cref=""RepositoryURL""/>.
-    /// </summary>
-    public const string Author = ""{REPOSITORY_AUTHOR}"";
-    /// <summary>
-    /// Development year(s).
-    /// </summary>
-    public const string Year = ""{year}"";
-    /// <summary>
-    /// The interpreter's copyright information.
-    /// </summary>
-    public const string Copyright = ""{copyright}"";
-    /// <summary>
-    /// The interpreter's current version.
-    /// </summary>
-    public static Version? InterpreterVersion {{ get; }} = Version.Parse(""{v2}"");
-    /// <summary>
-    /// The Git hash associated with the current build.
-    /// </summary>
-    public const string GitHash = ""{githash}"";
-    /// <summary>
-    /// The name of the GitHub repository associated with <see cref=""RepositoryURL""/>.
-    /// </summary>
-    public const string RepositoryName = ""{REPOSITORY_NAME}"";
-    /// <summary>
-    /// The URL of this project's GitHub repository.
-    /// </summary>
-    public const string RepositoryURL = ""{REPOSITORY_URL}"";
-    /// <summary>
-    /// The date and time of the current build ({now:yyyy-MM-dd HH:mm:ss.fff}).
-    /// </summary>
-    public static DateTime DateBuilt {{ get; }} = DateTime.FromFileTimeUtc(0x{now.ToFileTimeUtc():x16}L);
-}}
-");
+            p.WaitForExit();
         }
+        catch
+        {
+        }
+
+        if (string.IsNullOrWhiteSpace(githash))
+            githash = "<unknown git commit hash>";
+
+        string year = START_YEAR < now.Year ? $"{START_YEAR} - {now.Year}" : START_YEAR.ToString();
+        string copyright = $"Copyright © {year}, {REPOSITORY_AUTHOR}";
+
+        File.WriteAllText(verspath, $"{v2}\n{githash}");
+        File.WriteAllText(metapath, $$"""
+
+        //////////////////////////////////////////////////////////////////////////
+        // Auto-generated {{now:yyyy-MM-dd HH:mm:ss.fff}}                               //
+        // ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
+        //////////////////////////////////////////////////////////////////////////
+
+        using System.Reflection;
+        using System;
+
+        [assembly: AssemblyVersion("{{v2}}")]
+        [assembly: AssemblyFileVersion("{{v2}}")]
+        [assembly: AssemblyInformationalVersion("v.{{v2}}, commit: {{githash}}")]
+        [assembly: AssemblyCompany("{{REPOSITORY_AUTHOR}}")]
+        [assembly: AssemblyCopyright("{{copyright}}")]
+        [assembly: AssemblyProduct("{{REPOSITORY_NAME}} by {{REPOSITORY_AUTHOR}}")]
+        [assembly: AssemblyTitle("autoit3")]
+
+        /// <summary>
+        /// A global module containing some meta-data.
+        /// </summary>
+        public static class __module__
+        {
+            /// <summary>
+            /// The interpreter's author. This value is equal to the author of the GitHub repository associated with <see cref="RepositoryURL"/>.
+            /// </summary>
+            public const string Author = "{{REPOSITORY_AUTHOR}}";
+            /// <summary>
+            /// Development year(s).
+            /// </summary>
+            public const string Year = "{{year}}";
+            /// <summary>
+            /// The interpreter's copyright information.
+            /// </summary>
+            public const string Copyright = "{{copyright}}";
+            /// <summary>
+            /// The interpreter's current version.
+            /// </summary>
+            public static Version? InterpreterVersion { get; } = Version.Parse("{{v2}}");
+            /// <summary>
+            /// The Git hash associated with the current build.
+            /// </summary>
+            public const string GitHash = "{{githash}}";
+            /// <summary>
+            /// The name of the GitHub repository associated with <see cref="RepositoryURL"/>.
+            /// </summary>
+            public const string RepositoryName = "{{REPOSITORY_NAME}}";
+            /// <summary>
+            /// The URL of this project's GitHub repository.
+            /// </summary>
+            public const string RepositoryURL = "{{REPOSITORY_URL}}";
+            /// <summary>
+            /// The date and time of the current build ({{now:yyyy-MM-dd HH:mm:ss.fff}}).
+            /// </summary>
+            public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x{{now.ToFileTimeUtc():x16}}L);
+        }
+
+        """);
+        File.WriteAllText(appveyorpath, $"""
+        version: {v2}
+        image: Visual Studio 2022
+        configuration: Release
+        build:
+            project: new/AutoItInterpreter.sln
+            verbosity: minimal
+        """);
     }
 }
